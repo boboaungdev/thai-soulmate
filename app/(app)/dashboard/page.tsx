ec19ac80ed9e0eb13892f45904040a2cfacc96d8
Bo Bo <bobo@kkhay.com>
2026-07-28 15:16:43 +0700

    d

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -3,8 +3,14 @@
 export default function DashboardPage() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
+      <div className="flex items-center justify-between space-y-2">
+        <div>
+          <h1 className="text-2xl font-bold tracking-tight">User Dashboard</h1>
+          <p className="text-sm text-muted-foreground">
+            Manage all of my account information
+          </p>
+        </div>
+        <div className="flex items-center space-x-2"></div>
       </div>
     </main>
   )

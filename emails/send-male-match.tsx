14ab08cc37bb2e5f422b26d9d8229ac7f74fef53
Bo Bo <bobo@kkhay.com>
2026-07-31 02:31:12 +0700

    ff

--- emails/send-male-match.tsx
+++ emails/send-male-match.tsx
@@ -9,7 +9,7 @@ import {
   Preview,
   Section,
   Text,
-} from "@react-email/components"
+} from "react-email"
 import * as React from "react"
 
 import { APP_INFO, BASE_URL, CONTACT } from "@/constants"

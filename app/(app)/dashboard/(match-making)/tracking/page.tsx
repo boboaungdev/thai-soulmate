adc81b113ae758f2e17c71a70366c1e351a98d01
Bo Bo <bobo@kkhay.com>
2026-08-09 15:52:25 +0700

    d

--- app/(app)/dashboard/(match-making)/tracking/page.tsx
+++ app/(app)/dashboard/(match-making)/tracking/page.tsx
@@ -214,7 +214,7 @@ const SoulmateActions: React.FC<{
       {sendProfileButton}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="h-8 w-8 p-0">
+          <Button variant="outline" className="h-8 w-8 p-0">
             <span className="sr-only">Open menu</span>
             <MoreHorizontal className="h-4 w-4" />
           </Button>

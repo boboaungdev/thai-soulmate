7d8c4decf57ba128e62e73ac7b5c15651591244c
Bo Bo <bobo@kkhay.com>
2026-08-20 14:31:32 +0700

    test2

--- lib/browser.ts
+++ lib/browser.ts
@@ -1,10 +1,32 @@
+import fs from "node:fs"
 import puppeteer from "puppeteer-core"
 import chromium from "@sparticuz/chromium-min"
 
+// For linux
+const LOCAL_BRAVE_PATH = "/usr/bin/brave"
+
+let cachedExecutablePath: string | null = null
+
 export async function launchBrowser() {
+  if (process.env.NODE_ENV === "development") {
+    if (!fs.existsSync(LOCAL_BRAVE_PATH)) {
+      throw new Error(`Brave not found at ${LOCAL_BRAVE_PATH}`)
+    }
+
+    return puppeteer.launch({
+      executablePath: LOCAL_BRAVE_PATH,
+      headless: true,
+    })
+  }
+
+  if (!cachedExecutablePath) {
+    // This will download the browser if it's not already cached
+    cachedExecutablePath = await chromium.executablePath()
+  }
+
   return puppeteer.launch({
     args: chromium.args,
-    executablePath: await chromium.executablePath(),
-    headless: true,
+    executablePath: cachedExecutablePath,
+    headless: true, // Use modern headless mode
   })
 }

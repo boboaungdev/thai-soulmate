a1330302bf96e188eb6c52cec3d827069b248e00
Bo Bo <bobo@kkhay.com>
2026-08-29 23:14:39 +0700

    c

--- app/(app)/dashboard/(admin)/calendar/page.tsx
+++ app/(app)/dashboard/(admin)/calendar/page.tsx
@@ -4,12 +4,13 @@ import { CalendarView } from "./calendar-view"
 
 export default function Calendar() {
   return (
-    <div className="flex h-full flex-1 flex-col space-y-4">
+    <div className="flex h-full flex-1 flex-col space-y-6 p-4 lg:p-6">
       <div className="flex items-center justify-between space-y-2">
         <div>
           <h1 className="text-2xl font-bold tracking-tight">Calendar</h1>
           <p className="text-sm text-muted-foreground">
-            Tasks, events, register-interest appointments and all other schedules.
+            Tasks, events, register-interest appointments and all other
+            schedules.
           </p>
         </div>
       </div>

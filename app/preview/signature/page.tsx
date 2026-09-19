15fcc490b1fbd98ffbe2e6c5f379606f9dc06c2a
Bo Bo <bobo@kkhay.com>
2026-09-20 02:40:26 +0700

    d

--- app/preview/signature/page.tsx
+++ app/preview/signature/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react"
 import Link from "next/link"
 import Image from "next/image"
-import { EmailSignature, NewEmailSignature } from "@/features/shared/emails"
+import { EmailSignature1, NewEmailSignature } from "@/features/shared/emails"
 import { APP_INFO } from "@/constants"
 import { Check, Copy, Laptop, Smartphone, ExternalLink } from "lucide-react"
 
@@ -202,7 +202,7 @@ export default function EmailSignaturePreviewPage() {
 
               {/* Render Signature Component */}
               <div id="email-signature-render-target" className="pt-2">
-                <EmailSignature />
+                <EmailSignature1 />
               </div>
             </div>
           </div>
--- features/shared/emails/index.ts
+++ features/shared/emails/index.ts
@@ -1,5 +1,7 @@
 export * from "./member-email-layout"
 export * from "./email-signature"
 export { default as EmailSignature } from "./email-signature"
+export { default as EmailSignature1 } from "./email-signature-1"
 export { default as NewEmailSignature } from "./new-email-signature"
+
 export * from "./admin-notification-card"

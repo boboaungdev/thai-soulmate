0ab75289a8f953d2a97aa0439468291593f56d4d
Bo Bo <boboaungdev@gmail.com>
2026-09-20 16:51:51 +0700

    ff

--- lib/env.ts
+++ lib/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-  BASE_URL: 'https://thaisoulmate.org',
+  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
 
   DATABASE_URL: process.env.DATABASE_URL,
 
